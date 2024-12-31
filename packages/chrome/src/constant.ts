534752c927bdf6b643c9edb43f5f0542fd46d807
d_wessner <fwessner@electronicpartner.de>
2024-12-31 12:48:37 +0100

    fix duplicate const

--- packages/chrome/src/constant.ts
+++ packages/chrome/src/constant.ts
@@ -6,4 +6,4 @@ export const STORAGE_ACTIVE = 'wf-bm-active';
 export const STORAGE_PROJECTS = 'wf-bm-projects';
 export const STORAGE_ACTIVE_PROJECTS = 'wf-bm-active-projects';
 export const STORAGE_MANIFEST_PREFIX = 'wf-bm-manifest-';
-export const STORAGE_ACTIVE_REQUESTS = 'wf-bm-active';
+export const STORAGE_ACTIVE_REQUESTS = 'wf-bm-active-requests';
