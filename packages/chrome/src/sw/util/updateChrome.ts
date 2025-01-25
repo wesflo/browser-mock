fe285b84c947cbe8e9d4f6f271b86ee40862b4f7
d_wessner <fwessner@electronicpartner.de>
2025-01-25 16:47:20 +0100

    fix empty rule error on first run

--- packages/chrome/src/sw/util/updateChrome.ts
+++ packages/chrome/src/sw/util/updateChrome.ts
@@ -1,5 +1,6 @@
 import {buildRules} from "./buildRules";
 import Rule = chrome.declarativeNetRequest.Rule;
+import UpdateRuleOptions = chrome.declarativeNetRequest.UpdateRuleOptions;
 
 export const updateChrome = async (activePlugin: boolean) => {
     const allRules: Rule[] = await chrome.declarativeNetRequest.getDynamicRules();
@@ -9,10 +10,13 @@ export const updateChrome = async (activePlugin: boolean) => {
         rules = await buildRules();
     }
 
-    await chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: allRules.map((rule) => rule.id),
+    const options: UpdateRuleOptions = {
         addRules: rules
-    });
+    }
+
+    allRules && (options.removeRuleIds = allRules.map((rule) => rule.id))
+
+    await chrome.declarativeNetRequest.updateDynamicRules(options);
 
     console.log('Rules updated:', rules, await chrome.declarativeNetRequest.getDynamicRules());
 }
